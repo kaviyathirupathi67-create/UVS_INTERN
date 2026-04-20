2778aa33dd9744bb47ce56edeac91000c9e0f957
Kaviya Kaviya <kaviyathirupathi67@gmail.com>
2026-04-20 07:39:12 +0530

    BindingDemo

---
+++ BindingDemo/BindingDemo/View/StudentView.xaml.cs
@@ -0,0 +1,13 @@
+using BindingDemo.ViewModel;
+
+namespace BindingDemo.View
+{
+    public partial class StudentView : ContentPage
+    {
+        public StudentView(StudentViewModel studentViewModel)
+        {
+            InitializeComponent();
+            BindingContext = studentViewModel;
+        }
+    }
+}
---
+++ BindingDemo/BindingDemo/ViewModel/StudentViewModel.cs
@@ -0,0 +1,66 @@
+﻿using BindingDemo.Model;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BindingDemo.ViewModel
+{
+    public partial class StudentViewModel : ObservableObject
+    {
+        private Student _student;
+
+        public string _firstName;
+
+        public string FirstName
+        {
+            get => _firstName;
+            set {
+                if (_firstName != value)
+                {
+                    _firstName = value;
+                }
+            }
+        }
+        public string _lastName;
+        public string LastName
+        {
+            get => _lastName;
+            set
+            {
+                if (_lastName != value)
+                {
+                   _lastName = value;
+                }
+            }
+        }
+
+        public string _fullName = "Please Enter the Name";
+        
+
+        public string FullName
+        {
+            get => _fullName;
+            set
+            {
+                if (_fullName != value)
+                {
+                    _fullName = value;
+                    OnPropertyChanged(nameof(FullName));
+                }
+            }
+        }
+
+        public StudentViewModel()
+        {
+            _student = new Student(); 
+        }
+        [RelayCommand]
+         private void GenerateName()
+        {
+            FullName = $"Hello,{FirstName}{LastName} Relay Command Working!";
+        }
+    }
+}
+
